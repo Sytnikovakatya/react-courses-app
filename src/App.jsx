666ab8feaa6678bff73bcb1456e34d16148ec2eb
Kateryna Sytnikova <katerinafarina400@gmail.com>
2023-03-31 10:58:39 +0300

    Described router in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Header from './components/Header/Header';
 import Courses from './components/Courses/Courses';
+import CreateCourse from './components/CreateCourse/CreateCourse';
+import Registration from './components/Registration/Registration';
+import Login from './components/Login/Login';
+import CourseInfo from './components/CourseInfo/CourseInfo';
 
 function App() {
 	return (
-		<>
+		<Router>
 			<Header />
-			<Courses />
-		</>
+			<Routes>
+				<Route path='courses' element={<Courses />} />
+				<Route path='courses/add' element={<CreateCourse />} />
+				<Route path='courses/:courseId' element={<CourseInfo />} />
+				<Route path='registration' element={<Registration />} />
+				<Route path='login' element={<Login />} />
+			</Routes>
+		</Router>
 	);
 }
 
