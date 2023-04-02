fad759cc1ade44a16b2c667479f577723973b5ed
Kateryna Sytnikova <katerinafarina400@gmail.com>
2023-04-02 12:31:27 +0300

    Made an auth functionality for registration form

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import axios from '../api/axios';
 
 import Button from '../../common/Button/Button';
 import Input from '../../common/Input/Input';
@@ -8,13 +10,50 @@ import Form from 'react-bootstrap/Form';
 import { Container, InputGroup } from 'react-bootstrap';
 
 export default function Registration() {
+	const [userName, setUserName] = useState('');
+	const [userEmail, setUserEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const navigate = useNavigate();
+
+	const REGISTER_URL = '/register';
+
+	const submitRegistration = async (e) => {
+		e.preventDefault();
+		const newUser = {
+			name: userName,
+			email: userEmail,
+			password: password,
+		};
+
+		try {
+			const response = await axios.post(REGISTER_URL, JSON.stringify(newUser), {
+				headers: { 'Content-Type': 'application/json' },
+			});
+			console.log(JSON.stringify(response));
+
+			navigate('/login');
+			setUserName('');
+			setPassword('');
+			setUserEmail('');
+		} catch (err) {
+			if (!err?.response) {
+				throw new Error('No Server Response');
+			} else if (err.response?.status === 409) {
+				throw new Error('Username Taken');
+			} else {
+				throw new Error('Registration Failed');
+			}
+		}
+	};
+
 	return (
 		<>
 			<Container fluid='sm' className='w-50'>
 				<h1 className='pb-5 text-center'>Registration</h1>
 
-				<Form>
-					<Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+				<Form onSubmit={submitRegistration}>
+					<Form.Group className='mb-3'>
 						<Form.Label>Name</Form.Label>
 						<InputGroup className='mb-3'>
 							<Input
@@ -22,10 +61,11 @@ export default function Registration() {
 								labelText='username'
 								nameInput='User name'
 								id='name'
+								onChange={(e) => setUserName(e.target.value)}
 							/>
 						</InputGroup>
 					</Form.Group>
-					<Form.Group className='mb-3' controlId='exampleForm.ControlInput2'>
+					<Form.Group className='mb-3'>
 						<Form.Label>Email address</Form.Label>
 						<InputGroup className='mb-3'>
 							<Input
@@ -33,10 +73,11 @@ export default function Registration() {
 								labelText='email'
 								nameInput='User email'
 								id='email'
+								onChange={(e) => setUserEmail(e.target.value)}
 							/>
 						</InputGroup>
 					</Form.Group>
-					<Form.Group className='mb-3' controlId='exampleForm.ControllInput3'>
+					<Form.Group className='mb-3'>
 						<Form.Label>Password</Form.Label>
 						<InputGroup className='mb-3'>
 							<Input
@@ -44,12 +85,12 @@ export default function Registration() {
 								labelText='password'
 								nameInput='User password'
 								id='password'
+								onChange={(e) => setPassword(e.target.value)}
 							/>
 						</InputGroup>
 					</Form.Group>
+					<Button text='Registration' type='submit' />
 				</Form>
-
-				<Button text='Registration' type='submit' />
 
 				<p className='mt-5 text-center'>
 					If you have an account you can <Link to='/login'>Login</Link>
