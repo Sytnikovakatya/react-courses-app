a4a2f9c6fb15360a8a154fd879d3f0bf139316eb
Kateryna Sytnikova <katerinafarina400@gmail.com>
2023-04-03 12:56:51 +0300

    Create PrivateOutlet"

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import PrivateOutlet from './components/PrivateOutlet/PrivateOutlet';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Header from './components/Header/Header';
 import Courses from './components/Courses/Courses';
 import CreateCourse from './components/CreateCourse/CreateCourse';
 import Registration from './components/Registration/Registration';
@@ -13,13 +12,15 @@ import CourseInfo from './components/CourseInfo/CourseInfo';
 function App() {
 	return (
 		<Router>
-			<Header />
 			<Routes>
-				<Route path='courses' element={<Courses />} />
-				<Route path='courses/add' element={<CreateCourse />} />
-				<Route path='courses/:courseId' element={<CourseInfo />} />
-				<Route path='registration' element={<Registration />} />
+				<Route path='/' element={<Login />} />
 				<Route path='login' element={<Login />} />
+				<Route path='registration' element={<Registration />} />
+				<Route pathh='/' element={<PrivateOutlet />}>
+					<Route path='courses' element={<Courses />} />
+					<Route path='courses/add' element={<CreateCourse />} />
+					<Route path='courses/:courseId' element={<CourseInfo />} />
+				</Route>
 			</Routes>
 		</Router>
 	);
