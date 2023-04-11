67403c7682f3bbafd9b2c5003b2ce7bced3d276e
Kateryna Sytnikova <katerinafarina400@gmail.com>
2023-04-11 21:42:33 +0300

    Update Header with user data from store

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 
 import Logo from './components/Logo/Logo';
+import LogoutButton from '../Login/LogoutButton';
 
 export default function Header() {
+	const { user: currentUser } = useSelector((state) => state.auth);
+	const userData = JSON.parse(currentUser);
 	return (
 		<>
 			<Navbar className='m-4 shadow-sm'>
@@ -14,6 +18,14 @@ export default function Header() {
 						<Logo />
 					</Navbar.Brand>
 					<Navbar.Toggle />
+					{currentUser && (
+						<Navbar.Collapse className='justify-content-end'>
+							<Navbar.Text className='user-name me-3 fw-bold'>
+								{userData.user.name}
+							</Navbar.Text>
+							<LogoutButton />
+						</Navbar.Collapse>
+					)}
 				</Container>
 			</Navbar>
 		</>
