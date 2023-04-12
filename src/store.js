c05417b15695b868ddcef9dcbd3dd36b1c33ca38
Kateryna Sytnikova <katerinafarina400@gmail.com>
2023-04-12 22:35:33 +0300

    add new reducer to store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './features/authSlice';
+import authReducer from './features/authenticationSlice';
 import messageReducer from './features/messageSlice';
+import coursesReducer from './features/coursesSlice';
+import authorsReducer from './features/authorsSlice';
 
 const reducer = {
+	courses: coursesReducer,
+	authors: authorsReducer,
 	auth: authReducer,
 	message: messageReducer,
 };
