1d811a33e2cde8b414c7f135a02ab5036e7d74e9
Kateryna Sytnikova <katerinafarina400@gmail.com>
2023-04-14 13:37:56 +0300

    add statement to check data

--- src/helpers/getAuthorNames.js
+++ src/helpers/getAuthorNames.js
@@ -1,8 +1,13 @@
 export function getAuthorNames(listOfAuthorsIds, totalAuthorList) {
-	return listOfAuthorsIds
-		.map(
-			(authorId) =>
-				totalAuthorList.find((author) => author.id === authorId)?.name || 'N/A'
-		)
-		.join(', ');
+	if (totalAuthorList) {
+		return listOfAuthorsIds
+			.map(
+				(authorId) =>
+					totalAuthorList.find((author) => author.id === authorId)?.name ||
+					'N/A'
+			)
+			.join(', ');
+	} else {
+		return 'Loading';
+	}
 }
